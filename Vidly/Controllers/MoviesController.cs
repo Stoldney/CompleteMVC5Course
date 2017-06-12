065b07917c331fe1ff93ad08c301a23691f81187
Charles Middleton <charles.e.middleton@gmail.com>
2017-06-12 14:07:11 -0500

    Initial setup. Added Movie models, views, and controllers.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+	public class MoviesController : Controller
+	{
+		// GET: Movies
+		public ActionResult Random()
+		{
+			var movie = new Movie() { Name = "The Matrix" };
+
+			var customers = new List<Customer>
+			{
+				new Customer {Name = "Stephanie"},
+				new Customer {Name = "Charlemon"}
+			};
+
+			var viewModel = new RandomMovieViewModel
+			{
+				Movie = movie,
+				Customers = customers
+			};
+
+			return View(viewModel);			
+		}
+
+		public ActionResult Edit(int id)
+		{
+			return Content("id = " + id);
+		}
+
+		public ActionResult Index(int? pageIndex, string sortBy)
+		{
+			if (!pageIndex.HasValue)
+				pageIndex = 1;
+
+			if (string.IsNullOrEmpty(sortBy))
+				sortBy = "Name";
+
+			return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+		}
+
+		[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+		public ActionResult ByReleaseDate(int year, int month)
+		{
+			return Content(year + "/" + month);
+		}
+
+		
+	}
+}
