537a8feccf2002c17f3c859f267416ba2154fe0e
Charles Middleton <charles.e.middleton@gmail.com>
2017-06-16 01:11:14 -0500

    Populated customer table. Removed hardcoded customers and retrieve them from db

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 using Vidly.Models;
 
@@ -10,31 +11,33 @@ namespace Vidly.Controllers
 {
 	public class CustomersController : Controller
 	{
-		public ActionResult Index()
+		private ApplicationDbContext _context;
+
+		public CustomersController()
 		{
-			var customers = GetCustomers();
+			_context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
+		public ViewResult Index()
+		{
+			var customers = _context.Customers.Include(c => c.MembershipType);
 
 			return View(customers);
 		}
 
 		public ActionResult Details(int id = 1)
 		{
-			var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
 			if (customer == null)
 				return HttpNotFound();
 
 			return View(customer);
-		}
-
-		private IEnumerable<Customer> GetCustomers()
-		{
-			return new List<Customer>
-			{
-				new Customer { Id = 1, Name = "Charlemon Duboise"},
-				new Customer { Id = 2, Name = "Hans Gruber"},
-				new Customer { Id = 3, Name = "Baxter Brixton" }
-			};
-		}
+		}		
 	}
 }
