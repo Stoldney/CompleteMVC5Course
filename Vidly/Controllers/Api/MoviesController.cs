c01f98cccc7289bf4106aba5fdb799f218b03d86
Charles Middleton <charles.e.middleton@gmail.com>
2017-06-28 14:26:49 -0500

    Section 7 Excercise - Added datatables to movies list and implemented delete.

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -28,7 +29,10 @@ namespace Vidly.Controllers.Api
 		// GET /api/movies
 		public IHttpActionResult GetMovies()
 		{
-			var movieDtos = _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+			var movieDtos = _context.Movies
+									.Include(m => m.Genre)									
+									.ToList()
+									.Select(Mapper.Map<Movie, MovieDto>);
 			return Ok(movieDtos);
 		}
 
